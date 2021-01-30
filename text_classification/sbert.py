036d815f708051b9e45aa9079c976690d9fecaa1
Makeesy AI <makeesyai@gmail.com>
2021-01-30 19:22:28 +0000

    Update protobuf in requirements

--- text_classification/sbert.py
+++ text_classification/sbert.py
@@ -1,8 +1,13 @@
 # model_name: 'distilbert-base-nli-mean-tokens'
+# INSTALL: sentence-transformers
 # Sentences we want sentence embeddings for
-sentences = ['This framework generates embeddings for each input sentence',
-             'Sentences are passed as a list of string.',
-             'The quick brown fox jumps over the lazy dog.']
+sentences = [
+    'This framework generates embeddings for each input sentence',
+    'Sentences are passed as a list of string.',
+    'The quick brown fox jumps over the lazy dog.',
+    'The fox was running towards the jungle.',
+    'The dogs are following the fox.'
+]
 
 from sentence_transformers import SentenceTransformer, util
 
@@ -11,5 +16,5 @@ encodings = model.encode(sentences, convert_to_tensor=True)
 print(type(encodings))
 print(encodings.size())
 
-scores = util.pytorch_cos_sim(encodings, encodings)
-print(scores)
+# scores = util.pytorch_cos_sim(encodings, encodings)
+# print(scores)
