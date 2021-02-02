a2f0f32466dfb1f54c05bd18d955bea31512c2d4
Makeesy AI <makeesyai@gmail.com>
2021-02-02 21:00:20 +0000

    fix the loss function

--- text_classification/sbert.py
+++ text_classification/sbert.py
@@ -6,5 +6,16 @@ sentences = [
     'Sentences are passed as a list of string.',  # 0
     'The quick brown fox jumps over the lazy dog.',  # 1
     'The lazy dog is also jumping.',  # 1
-    'The fox and the dog are playing.',  # 1
+    'The fox and the dog are playing.'  # 1
 ]
+
+from sentence_transformers import SentenceTransformer
+
+encoder = SentenceTransformer('distilbert-base-nli-mean-tokens')
+embedding = encoder.encode(sentences)
+print(embedding.shape)
+
+from sentence_transformers import util
+
+scores = util.pytorch_cos_sim(embedding, embedding)
+print(scores)
