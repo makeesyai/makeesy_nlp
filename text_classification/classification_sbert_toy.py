160677eaf13e88a8a2dd6fb6d7239098277fb9cf
Makeesy AI <makeesyai@gmail.com>
2021-02-02 21:09:00 +0000

    refactor the code for variables

--- text_classification/classification_sbert_toy.py
+++ text_classification/classification_sbert_toy.py
@@ -17,12 +17,12 @@ from torch.nn import functional as F
 
 
 class Classifier(nn.Module):
-    def __init__(self, embedding_size, num_labels, dropout=0.7):
+    def __init__(self, embedding_size, n_labels, dropout=0.01):
         super(Classifier, self).__init__()
         self.embedding_size = embedding_size
-        self.num_labels = num_labels
+        self.num_labels = n_labels
         self.dropout = nn.Dropout(dropout)
-        self.ff = nn.Linear(embedding_size, num_labels)
+        self.ff = nn.Linear(embedding_size, n_labels)
 
     def forward(self, x):
         tensor = self.dropout(x)
@@ -32,27 +32,27 @@ class Classifier(nn.Module):
 
 # Sentences we want sentence embeddings for
 sentences = [
-    'This framework generates embeddings for each input sentence',
-    'Sentences are passed as a list of string.',
-    'The quick brown fox jumps over the lazy dog.',
-    'The lazy dog is also jumping.',
-    'The fox and the dog are playing.',
+    'This framework generates embeddings for each input sentence',  # 0
+    'Sentences are passed as a list of string.',  # 0
+    'The quick brown fox jumps over the lazy dog.',  # 1
+    'The lazy dog is also jumping.',  # 1
+    'The fox and the dog are playing.'  # 1
 ]
-
 labels = torch.tensor([0, 0, 1, 1, 1])
 
-# embedder = SentenceTransformer('distilbert-base-nli-mean-tokens')
-embedder = SentenceTransformer('quora-distilbert-multilingual')
+# encoder = SentenceTransformer('distilbert-base-nli-mean-tokens')
+encoder = SentenceTransformer('quora-distilbert-multilingual')
 start_time = time.time()
-sentence_embeddings = embedder.encode(sentences, convert_to_tensor=True)
-print(f'The encoding time:{time.time() - start_time}')
 
-use_cuda = True if torch.cuda.is_available() else False
-device = 'cuda:0' if use_cuda else 'cpu'
+sentence_embeddings = encoder.encode(sentences, convert_to_tensor=True)
+print(f'The encoding time:{time.time() - start_time}')
 
 num_sentence, embedding_dim = sentence_embeddings.size()
 num_labels = labels.unique().shape[0]
 classifier = Classifier(embedding_dim, num_labels, dropout=0.01)
+
+use_cuda = True if torch.cuda.is_available() else False
+device = 'cuda:0' if use_cuda else 'cpu'
 
 # Move model and data to device
 classifier.to(device)
@@ -68,8 +68,8 @@ loss_fn = nn.CrossEntropyLoss()
 
 for e in range(10):
     optimizer.zero_grad()
-    logits, prob = classifier(sentence_embeddings)
-    loss = loss_fn(logits, labels)
+    model_outputs, prob = classifier(sentence_embeddings)
+    loss = loss_fn(model_outputs, labels)
     print(loss.item())
     loss.backward()
     optimizer.step()
@@ -82,10 +82,9 @@ test_sentences = [
 ]
 test_labels = [0, 1, 0, 1]
 
-test_sentence_embeddings = embedder.encode(test_sentences,
-                                           convert_to_tensor=True)
+test_sentence_embeddings = encoder.encode(test_sentences, convert_to_tensor=True)
 test_sentence_embeddings = test_sentence_embeddings.to(device)
 
 with torch.no_grad():
-    logits, prob = classifier(test_sentence_embeddings)
+    model_outputs, prob = classifier(test_sentence_embeddings)
     print(torch.argmax(prob, dim=-1))
