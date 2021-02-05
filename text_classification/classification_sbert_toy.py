7857961493f7a9fa50f947d70b8357bc79c26b69
Makeesy AI <makeesyai@gmail.com>
2021-02-05 22:19:08 +0000

    Add prediction code.

--- text_classification/classification_sbert_toy.py
+++ text_classification/classification_sbert_toy.py
@@ -38,6 +38,7 @@ sentences = [
     'The lazy dog is also jumping.',  # 1
     'The fox and the dog are playing.'  # 1
 ]
+
 labels = torch.tensor([0, 0, 1, 1, 1])
 
 encoder = SentenceTransformer('distilbert-base-nli-mean-tokens')
@@ -53,13 +54,25 @@ with torch.no_grad():
     model_outputs, prob = classifier(sentence_embeddings)
     print(torch.argmax(prob, dim=-1))
 
-use_cuda = True if torch.cuda.is_available() else False
-device = 'cuda:0' if use_cuda else 'cpu'
+
+test_sentences = [
+    'The sentence is used here has good embeddings.',
+    'The boy is playing with the dog and jumping in joy.',
+    # 'यहाँ वाक्य का इस्तेमाल किया गया है जिसमें अच्छी एम्बेडिंग है।',
+    # 'डॉग्स के साथ खेलता लड़का और खुशी से उछलता।'
+]
+test_labels = [0, 1, 0, 1]
+
+test_sentence_embeddings = encoder.encode(test_sentences, convert_to_tensor=True)
+
+# use_cuda = True if torch.cuda.is_available() else False
+# device = 'cuda:0' if use_cuda else 'cpu'
 
 # Move model and data to device
-classifier.to(device)
-sentence_embeddings = sentence_embeddings.to(device)
-labels = labels.to(device)
+# classifier.to(device)
+# sentence_embeddings = sentence_embeddings.to(device)
+# test_sentence_embeddings = test_sentence_embeddings.to(device)
+# labels = labels.to(device)
 
 optimizer = optim.Adam(classifier.parameters())
 loss_fn = nn.CrossEntropyLoss()
@@ -71,17 +84,6 @@ for e in range(10):
     print(loss.item())
     loss.backward()
     optimizer.step()
-
-test_sentences = [
-    'The sentence is used here has good embeddings.',
-    'The boy is playing with the dog and jumping in joy.',
-    # 'यहाँ वाक्य का इस्तेमाल किया गया है जिसमें अच्छी एम्बेडिंग है।',
-    # 'डॉग्स के साथ खेलता लड़का और खुशी से उछलता।'
-]
-test_labels = [0, 1, 0, 1]
-
-test_sentence_embeddings = encoder.encode(test_sentences, convert_to_tensor=True)
-test_sentence_embeddings = test_sentence_embeddings.to(device)
 
 with torch.no_grad():
     model_outputs, prob = classifier(test_sentence_embeddings)
