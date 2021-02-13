5e1f9b67de342085fa0bb45d88a8ed6b0ebc2ec7
Makeesy AI <makeesyai@gmail.com>
2021-02-13 16:04:52 +0000

    Add training data for spam and tweet classification

--- text_classification/classification_sbert.py
+++ text_classification/classification_sbert.py
@@ -20,8 +20,12 @@ Install pandas (data loading)
 """
 import time
 
+import pandas
 import torch
 from sentence_transformers import SentenceTransformer
+from sklearn.metrics import classification_report
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
 from torch import nn, optim, tensor
 from torch.nn import functional as F
 
@@ -42,25 +46,26 @@ class Classifier(nn.Module):
         return tensor, F.softmax(tensor, dim=-1)
 
 
-# Sentences we want sentence embeddings for
-sentences = [
-    'This framework generates embeddings for each input sentence',  # 0
-    'Sentences are passed as a list of string.',  # 0
-    'The quick brown fox jumps over the lazy dog.',  # 1
-    'The lazy dog is also jumping.',  # 1
-    'The fox and the dog are playing.'  # 1
-]
+data_df = pandas.read_csv("../data/spam/SPAM text message 20170820 - Data.csv")
+data_df.dropna(inplace=True)
+data_df.drop_duplicates(inplace=True)
+data_df.rename(columns={
+    "Category": "labels",
+    "Message": "text"
+}, inplace=True)
 
-labels = torch.tensor([0, 0, 1, 1, 1])
+le = LabelEncoder()
+le.fit(data_df.labels)
+data_df["labels"] = le.transform(data_df.labels)
 
-test_sentences = [
-    'The sentence is used here has good embeddings.',  # 0
-    'The boy is playing with the dog and jumping in joy.', # 1
-    'यहाँ वाक्य का इस्तेमाल किया गया है जिसमें अच्छी एम्बेडिंग है।',  # 0 (MT Hindi Language)
-    'डॉग्स के साथ खेलता लड़का और खुशी से उछलता।'  # 1 (MT Hindi Language)
-]
+train_x, test_x, train_y, test_y = \
+    train_test_split(data_df.text, data_df.labels, stratify=data_df.labels, test_size=0.15)
 
-test_labels = tensor([0, 1, 0, 1])
+sentences = train_x.tolist()
+test_sentences = test_x.tolist()
+
+labels = torch.tensor(train_y.tolist())
+test_labels = torch.tensor(test_y.tolist())
 
 # encoder = SentenceTransformer('distilbert-base-nli-mean-tokens')
 encoder = SentenceTransformer('quora-distilbert-multilingual')
@@ -70,7 +75,6 @@ embedding = encoder.encode(sentences, convert_to_tensor=True)
 test_sentences_embedding = encoder.encode(test_sentences, convert_to_tensor=True)
 print(f"Encoding completed in {time.time() - start} seconds.")
 
-
 num_samples, embeddings_dim = embedding.size()
 n_labels = labels.unique().shape[0]
 
@@ -79,7 +83,7 @@ classifier = Classifier(embeddings_dim, n_labels, dropout=0.01)
 optimizer = optim.Adam(classifier.parameters())
 loss_fn = nn.CrossEntropyLoss()
 
-for e in range(10):
+for e in range(200):
     optimizer.zero_grad()
     model_output, prob = classifier(embedding)
     loss = loss_fn(model_output, labels)
@@ -87,10 +91,8 @@ for e in range(10):
     optimizer.step()
     print(loss.item())
 
-
 with torch.no_grad():
     model_output, prob = classifier(test_sentences_embedding)
-    print(model_output, prob)
-    print(f'True Labels: {test_labels}')
-    print(f'Predicted Labels:{torch.argmax(prob, dim=-1)}')
-
+    predictions = torch.argmax(prob, dim=-1)
+    results = classification_report(predictions, test_labels)
+    print(results)
