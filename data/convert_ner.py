e785ed8a73e0981f54d23b4a556b3f9e1f030c15
Makeesy AI <makeesyai@gmail.com>
2021-02-14 21:57:22 +0000

    Add data for NER

--- data/convert_ner.py
+++ data/convert_ner.py
@@ -1,6 +1,16 @@
+import numpy
 import pandas as pd
 
 data = pd.read_csv('../data/ner/ner_dataset.csv')
 
 data = data.fillna(method='ffill')
-print(data.tail())
+agg_func = lambda s: [(w, p, t) for w, p, t in zip(s["Word"].values.tolist(),
+                                                   s["POS"].values.tolist(),
+                                                   s["Tag"].values.tolist())]
+grouped = data.groupby(["Sentence #"]).apply(agg_func)
+sentences = [s for s in grouped]
+X = [' '.join([w[0] for w in s]) for s in sentences]
+y = [' '.join([w[2] for w in s]) for s in sentences]
+data = zip(X, y)
+df = pd.DataFrame(columns={'text', 'labels'}, data=data)
+df.to_csv('ner/ner.csv', index=False)
