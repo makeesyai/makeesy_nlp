483c17e486688a976960cdc5053355548ba174b8
Makeesy AI <makeesyai@gmail.com>
2021-02-14 13:29:59 +0000

    Add code for tutorial 6

--- token_classification/sbert.py
+++ token_classification/sbert.py
@@ -1,41 +1,52 @@
 import numpy
 import torch
+from sentence_transformers import SentenceTransformer
+from transformers import XLNetTokenizer, T5Tokenizer, GPT2Tokenizer
 
 sentences = [
-    'This framework generates embeddings for each input sentence',  # 0
-    'Sentences are passed as a list of string.',  # 0
-    'The quick brown fox jumps over the lazy dog.',  # 1
-    'The lazy dog is also jumping.',  # 1
-    'The fox and the dog are playing.'  # 1
+    'This framework generates embeddings for each input sentence .',  # 0
+    'Sentences are passed as a list of string .',  # 0
+    'The quick brown fox jumps over the lazy dog .',  # 1
+    'The lazy dog is also jumping .',  # 1
+    'The fox and the dog are playing .'  # 1
 ]
-
-from sentence_transformers import SentenceTransformer
 
 # encoder = SentenceTransformer('distilbert-base-nli-mean-tokens')
 encoder = SentenceTransformer('paraphrase-xlm-r-multilingual-v1')
-embedding = encoder.encode(sentences[0],
-                           output_value='token_embeddings',
-                           convert_to_tensor=True)
+embeddings = encoder.encode(sentences,
+                            output_value='token_embeddings',
+                            convert_to_tensor=True)
 tokenizer = encoder.tokenizer
-offset = 1
+
+# Most models have an initial BOS/CLS token, except for XLNet, T5 and GPT2
+begin_offset = 1
+if type(tokenizer) == XLNetTokenizer:
+    begin_offset = 0
+if type(tokenizer) == T5Tokenizer:
+    begin_offset = 0
+if type(tokenizer) == GPT2Tokenizer:
+    begin_offset = 0
+
 sentence_embeddings = []
-pooling = 'first'
-for index, emb in enumerate(embedding):
-    print(emb.size())
-    start_sub_token = offset
+pooling = 'mean'
+for embedding, sentence in zip(embeddings, sentences):
+    start_sub_token = begin_offset
     token_embeddings = []
-    for word in sentences[index].split():
+    for word in sentence.split():
         sub_tokens = tokenizer.tokenize(word)
         num_sub_tokens = len(sub_tokens)
-        all_embeddings = embedding[index][start_sub_token:start_sub_token + num_sub_tokens]
-        start_sub_token += num_sub_tokens
+        all_embeddings = embedding[start_sub_token:start_sub_token + num_sub_tokens]
+
         if pooling == 'first':
             final_embeddings = torch.mean(all_embeddings, dim=0)
         elif pooling == 'last':
-            final_embeddings = all_embeddings[0]
+            final_embeddings = all_embeddings[-1]
         else:
             final_embeddings = all_embeddings[0]
+
         token_embeddings.append(final_embeddings)
+        start_sub_token += num_sub_tokens
+
     sentence_embeddings.append(torch.stack(token_embeddings))
 
 # This will create a array of pointers to variable length tensors
