2aa7ff217567d626d6850d945dbfbd70fa78059d
Makeesy AI <makeesyai@gmail.com>
2021-03-07 13:19:38 +0000

    Update code for the tutorial 7

--- token_classification/sbert.py
+++ token_classification/sbert.py
@@ -17,23 +17,24 @@ embeddings = encoder.encode(sentences,
 tokenizer = encoder.tokenizer
 
 # Most models have an initial BOS/CLS token, except for XLNet, T5 and GPT2
-begin_offset = 1
+offset = 1
 if type(tokenizer) == XLNetTokenizer:
-    begin_offset = 0
+    offset = 0
 if type(tokenizer) == T5Tokenizer:
-    begin_offset = 0
+    offset = 0
 if type(tokenizer) == GPT2Tokenizer:
-    begin_offset = 0
+    offset = 0
 
 sentence_embeddings = []
 pooling = 'mean'
 for embedding, sentence in zip(embeddings, sentences):
+    subword_offset = offset
     true_embeddings = []
     words = sentence.split()
     for word in words:
         sub_tokens = tokenizer.tokenize(word)
         num_sub_tokens = len(sub_tokens)
-        all_embeddings = embedding[begin_offset: begin_offset + num_sub_tokens]
+        all_embeddings = embedding[subword_offset: subword_offset + num_sub_tokens]
 
         if pooling == 'first':
             final_embeddings = torch.mean(all_embeddings, dim=0)
@@ -43,7 +44,7 @@ for embedding, sentence in zip(embeddings, sentences):
             final_embeddings = all_embeddings[0]
 
         true_embeddings.append(final_embeddings)
-        begin_offset += num_sub_tokens
+        subword_offset += num_sub_tokens
 
     sentence_embeddings.append(torch.stack(true_embeddings))
 
